36cf8f22edac8e11a7cf0005fc82dc93084dd51f
yunwansu <yunwansu96@gmail.com>
2016-07-25 22:10:25 +0900

    test

---
+++ src/test/scala/hello_world.scala
@@ -0,0 +1,17 @@
+/**
+  * Created by lg on 2016-07-25.
+  */
+class Stack[T]{
+  var elems : List[T] = Nil
+  def push(x : T) {elems = x :: elems}
+  def top : T = elems.head
+  def pop  { elems = elems.tail}
+}
+
+object hello_world extends App{
+  var st = new Stack[Int]
+  st.push(10)
+  st.push(20)
+  println(st.top)
+  println(st.top)
+}
