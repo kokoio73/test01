88dd35c4b7d9600f116035496ff69869bffaf2f6
yunwansu <yunwansu96@gmail.com>
2016-07-26 11:07:07 +0900

    1
    1

---
+++ src/sum.scala
@@ -0,0 +1,22 @@
+/**
+  * Created by Ku on 2016-07-22.
+  */
+object sum extends App{
+  class Point(var x : Int, val y : Int){
+    var xc : Int = x
+    val yc : Int = y
+
+    var i :
+
+  }
+  var c = new Point(10, 20)
+
+  println(c.x)
+  println(c.y)
+
+  heh
+}
+
+object heh{
+  println(sum.c.xc)
+}
