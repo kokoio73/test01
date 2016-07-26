44b858fa0b57d8829c11f2e5bd0514f3998ac26d
yunwansu <yunwansu96@gmail.com>
2016-07-26 11:15:39 +0900

    a
    v

--- src/sum.scala
+++ src/sum.scala
@@ -5,15 +5,11 @@ object sum extends App{
   class Point(var x : Int, val y : Int){
     var xc : Int = x
     val yc : Int = y
-
-    var i :
-
   }
   var c = new Point(10, 20)
 
   println(c.x)
   println(c.y)
-
   heh
 }
 
