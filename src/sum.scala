a314ff600123fd00abf4df76d86c108e7661565a
yunwansu <yunwansu96@gmail.com>
2016-07-26 14:10:27 +0900

    abcde

--- src/sum.scala
+++ src/sum.scala
@@ -2,17 +2,30 @@
   * Created by Ku on 2016-07-22.
   */
 object sum extends App{
-  class Point(var x : Int, val y : Int){
-    var xc : Int = x
-    val yc : Int = y
+  case class ListNode[+T](h : T, t : ListNode[T]){
+    def head : T = h
+    def tail : ListNode[T] = t
+    def prepend[U >: T](elem : U) : ListNode[U] =
+      ListNode(elem, this)
+    def next: ListNode[T] = {
+     tail
+    }
   }
-  var c = new Point(10, 20)
+  def loop(body : =>Unit): Unit = {
+    try {
+      while (true) {
+        body
+      }
+    }catch{
+      case a : NullPointerException =>
+    }
+  }
+  var empty : ListNode[Null] = ListNode(null, null)
+  var strList : ListNode[String] = empty.prepend("hello").prepend("world")
+  var anyList : ListNode[Any] = strList.prepend(12345)
 
-  println(c.x)
-  println(c.y)
-  heh
-}
-
-object heh{
-  println(sum.c.xc)
+  loop{
+    println(anyList.head)
+    anyList = anyList next
+  }
 }
